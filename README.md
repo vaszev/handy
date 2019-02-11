06cbf319f9a947ecae2fd2d1cff7c8a933fd7409
Roli <roland.bodnar.80@gmail.com>
2019-02-11 15:03:39 +0100

    - readme modified - twig extension added

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ $ composer require vaszev/handy-bundle
 
 ### Contains
 
+###### Handy service methods
+
 - entityCheck *(check for object's id property)*
 - passwordEncode *(user's tool)*
 - passwordVerify *(user's tool)*
@@ -35,4 +37,31 @@ $ composer require vaszev/handy-bundle
 - urlResponse *(web tool)*
 - getClientIp *(web tool)*
 - filenameCheck *(filesystem tool)*
+
+###### Handy twig filters & functions
+
+- rndGen *(numeric tool)*
+- loremIpsum *(text tool)*
+- entityCheck *(check for object's id property)*
+- friendlyFilter *(text tool)*
+- minutesTimeFilter *(date tool)*
+- dayNameFilter *(date tool)*
+- priceFilter *(numeric tool)*
+- imgSizeFilter *(image tool)*
+- imgSizeFilterKept *(image tool)*
+- metricToImperial *(numeric tool)*
+- countryFilter *(text tool)*
+- ordinal *(numeric tool)*
+- strPos *(text tool)*
+- strReplace *(text tool)*
+- numberScale *(numeric tool)*
+- autoPunctuation *(text tool)*
+- quotation *(text tool)*
+- resolution *(image tool)*
+- secret *(text tool)*
+- strPad *(text tool)*
+- br2nl *(text tool)*
+- joinByKey *(data tool)*
+
+
 
---
+++ Twig/HandyExtension.php
@@ -0,0 +1,381 @@
+<?php
+
+namespace Vaszev\HandyBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Intl\Intl;
+use Twig\Extension\AbstractExtension;
+use Vaszev\HandyBundle\Service\Handy;
+
+class HandyExtension extends AbstractExtension {
+
+  private $handy;
+  private $container;
+
+
+
+  /**
+   * HandyExtension constructor.
+   * @param Handy $handy
+   * @param ContainerInterface $container
+   */
+  public function __construct(Handy $handy, ContainerInterface $container) {
+    $this->handy = $handy;
+    $this->container = $container;
+  }
+
+
+
+  public function getFilters() {
+    return [
+        new \Twig_SimpleFilter('joinByKey', [$this, 'joinByKey']),
+        new \Twig_SimpleFilter('secret', [$this, 'secret']),
+        new \Twig_SimpleFilter('minutesTime', [$this, 'minutesTimeFilter']),
+        new \Twig_SimpleFilter('dayName', [$this, 'dayNameFilter']),
+        new \Twig_SimpleFilter('price', [$this, 'priceFilter']),
+        new \Twig_SimpleFilter('imgSize', [$this, 'imgSizeFilter']),
+        new \Twig_SimpleFilter('imgSizeKept', [$this, 'imgSizeFilterKept']),
+        new \Twig_SimpleFilter('friendly', [$this, 'friendlyFilter']),
+        new \Twig_SimpleFilter('entityCheck', [$this, 'entityCheck']),
+        new \Twig_SimpleFilter('metricToImperial', [$this, 'metricToImperial']),
+        new \Twig_SimpleFilter('country', [$this, 'countryFilter']),
+        new \Twig_SimpleFilter('ordinal', [$this, 'ordinal']),
+        new \Twig_SimpleFilter('strPos', [$this, 'strPos']),
+        new \Twig_SimpleFilter('strReplace', [$this, 'strReplace']),
+        new \Twig_SimpleFilter('strPad', [$this, 'strPad']),
+        new \Twig_SimpleFilter('verifiedDefault', [$this, 'verifiedDefault']),
+        new \Twig_SimpleFilter('numberScale', [$this, 'numberScale']),
+        new \Twig_SimpleFilter('autoPunctuation', [$this, 'autoPunctuation']),
+        new \Twig_SimpleFilter('quotation', [$this, 'quotation'], ['is_safe' => ['html']]),
+        new \Twig_SimpleFilter('resolution', [$this, 'resolution']),
+        new \Twig_SimpleFilter('br2nl', [$this, 'br2nl']),
+    ];
+  }
+
+
+
+  public function getFunctions() {
+    return [
+        new \Twig_SimpleFunction('lorem', [$this, 'loremIpsum']),
+        new \Twig_SimpleFunction('rnd', [$this, 'rndGen']),
+    ];
+  }
+
+
+
+  public function rndGen($start = 0, $end = 100, $float = false) {
+    $tmp = [];
+    $tmp[] = (integer)$start;
+    $tmp[] = (integer)$end;
+    sort($tmp, SORT_NUMERIC);
+    $rnd = rand($tmp[0], $tmp[1]);
+    if ($float) {
+      $d = rand(1, 99) / 100;
+      $rnd += $d;
+    }
+
+    return $rnd;
+  }
+
+
+
+  public function loremIpsum($words = 1, $onlyalpha = false, $html = false) {
+    $sample = $this->handy->loremIpsum(100, true);
+
+    $tags = ['h2', 'h3', 'h4', 'strong', 'span', 'underline', 'p', 'em', 'a'];
+    $tmp = [];
+
+    $arr = explode(" ", $sample);
+    for ($w = 0; $w < ($words); $w++) {
+      shuffle($arr);
+      $first = $arr[0];
+      if ($onlyalpha) {
+        $tmp[] = preg_replace("/[^a-zA-Z]+/", "", $first);
+      } else {
+        $tmp[] = $first;
+      }
+      if ($html) {
+        shuffle($tags);
+        $tag = $tags[0];
+        $tmp[] = '<' . ($tag == "a" ? 'a href="javascript:void(0);"' : $tag) . '>' . $first . '</' . $tag . '>';
+      }
+    }
+    shuffle($tmp);
+
+    return implode(" ", $tmp);
+  }
+
+
+
+  public function entityCheck($entity) {
+    return $this->handy->entityCheck($entity);
+  }
+
+
+
+  public function friendlyFilter($str, $default = 'untitled') {
+    $str = trim($this->handy->friendlyFilter($str));
+    $str = ($str ? $str : $default);
+
+    return $str;
+  }
+
+
+
+  public function minutesTimeFilter($number) {
+    $hours = floor($number / 60);
+    $minutes = $number % 60;
+    $str = str_pad($hours, 2, '0', STR_PAD_LEFT) . ':' . str_pad($minutes, 2, '0', STR_PAD_LEFT);
+
+    return $str;
+  }
+
+
+
+  public function dayNameFilter($number) {
+    $days = [1 => 'monday', 2 => 'tuesday', 3 => 'wednesday', 4 => 'thursday', 5 => 'friday', 6 => 'saturday', 7 => 'sunday',];
+
+    return $days[$number];
+  }
+
+
+
+  public function priceFilter($number, $decimals = 0, $decPoint = '.', $thousandsSep = ',', $currency = '$') {
+    $price = number_format($number, $decimals, $decPoint, $thousandsSep);
+    $price = $currency . $price;
+
+    return $price;
+  }
+
+
+
+  public function imgSizeFilter($path, $size = 'small', $crop = false) {
+    $outer = strpos($path, 'http');
+    if ($outer === false) {
+      // not outer
+    } else {
+      // outer link, start with http
+      return $path;
+    }
+    $rootDir = $this->container->get('kernel')->getRootDir();
+    $docPath = trim($this->container->getParameter('vaszev_handy.docs'), '/');
+    // $defaultImage = __DIR__ . '/' . $this->container->getParameter('vaszev_commons.default_image');
+    $defaultImage = $this->container->getParameter('vaszev_handy.default_image');
+    $defaultImageNewName = 'default-transparent.png';
+    $defaultImageDestination = $rootDir . '/../web/' . $docPath . '/' . $defaultImageNewName;
+    // copy default image if not exists
+    if (!file_exists($defaultImageDestination)) {
+      copy($defaultImage, $defaultImageDestination);
+    }
+    $unfold = explode('/', $path);
+    $fileStr = end($unfold);
+    $originalUrl = $docPath . '/' . $fileStr;
+    $resizedUrl = $docPath . '/' . $size . ($crop ? '-cropped' : '') . '/' . $fileStr;
+    // pre-check for image, get default is it fails
+    $originalImageSize = @getimagesize($originalUrl);
+    if (empty($originalImageSize)) {
+      // not an image
+      $originalUrl = $docPath . '/' . $defaultImageNewName;
+      $resizedUrl = $docPath . '/' . $size . ($crop ? '-cropped' : '') . '/' . $defaultImageNewName;
+    }
+    // finally, get that image with correct size
+    if (!file_exists($resizedUrl)) {
+      $resizedUrl = $this->handy->getImageVariant($originalUrl, $size, $crop);
+    } else {
+      $resizedUrl = '/' . $resizedUrl;
+    }
+
+    return $resizedUrl;
+  }
+
+
+
+  public function imgSizeFilterKept($path, $size = 'small') {
+    error_reporting(E_ERROR);
+    $outer = strpos($path, 'http');
+    if ($outer === false) {
+      // not outer
+    } else {
+      // outer link, start with http
+      return $path;
+    }
+    $rootDir = $this->container->get('kernel')->getRootDir();
+    $docPath = trim($this->container->getParameter('vaszev_handy.docs'), '/');
+    // $defaultImage = __DIR__ . '/' . $this->container->getParameter('vaszev_commons.default_image');
+    $defaultImage = $this->container->getParameter('vaszev_handy.default_image');
+    $defaultImageNewName = 'default-transparent.png';
+    $defaultImageDestination = $rootDir . '/../web/' . $docPath . '/' . $defaultImageNewName;
+    // copy default image if not exists
+    if (!file_exists($defaultImageDestination)) {
+      copy($defaultImage, $defaultImageDestination);
+    }
+    $unfold = explode('/', $path);
+    $fileStr = end($unfold);
+    $originalUrl = $docPath . '/' . $fileStr;
+    $resizedUrl = $docPath . '/' . $size . '-kept' . '/' . $fileStr;
+    // pre-check for image, get default is it fails
+    $originalImageSize = @getimagesize($originalUrl);
+    if (empty($originalImageSize)) {
+      // not an image
+      $originalUrl = $docPath . '/' . $defaultImageNewName;
+      $resizedUrl = $docPath . '/' . $size . '-kept' . '/' . $defaultImageNewName;
+    }
+    // finally, get that image with correct size
+    if (!file_exists($resizedUrl)) {
+      $resizedUrl = $this->handy->getImageVersion($originalUrl, $size);
+    } else {
+      $resizedUrl = '/' . $resizedUrl;
+    }
+
+    return $resizedUrl;
+
+  }
+
+
+
+  public function metricToImperial($cm = 0) {
+    return $this->handy->metricToImperial($cm);
+  }
+
+
+
+  public function countryFilter($countryCode) {
+    $c = Intl::getRegionBundle()->getCountryName($countryCode);
+
+    return ($c ? $c : $countryCode);
+  }
+
+
+
+  public function ordinal($number) {
+    $ends = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+
+    if ((($number % 100) >= 11) && (($number % 100) <= 13)) {
+      return $number . 'th';
+    } else {
+      return $number . $ends[$number % 10];
+    }
+  }
+
+
+
+  public function strPos($string, $findMe) {
+    if (empty($string) || empty($findMe)) {
+      return null;
+    }
+
+    return stripos($string, $findMe);
+  }
+
+
+
+  public function strReplace($string, $findMe, $replace) {
+    $ret = str_ireplace($findMe, $replace, $string);
+
+    return $ret;
+  }
+
+
+
+  public function numberScale($number, $decimal = 1) {
+    return $this->handy->numberScale($number, $decimal);
+  }
+
+
+
+  public function autoPunctuation($string = null, $prefix = null, $postfix = null) {
+    if (empty($string)) {
+      return null;
+    }
+    $string = trim($string);
+    $end = substr($string, -1);
+    $chk = ctype_alnum($end);
+    $txt = $prefix . ($chk ? $string . '.' : $string) . $postfix;
+
+    return $txt;
+  }
+
+
+
+  public function quotation($string, $stripTags = true) {
+    $string = html_entity_decode($string);
+    $string = nl2br($string);
+    if ($stripTags) {
+      $string = strip_tags($string);
+    }
+    $string = html_entity_decode($string, ENT_QUOTES);
+    $string = str_replace("\n", ' ', $string);
+    $string = str_replace("\r", '', $string);
+    $string = str_replace('"', "'", $string);
+
+    return $string;
+  }
+
+
+
+  public function resolution($path, $type = 'width') {
+    try {
+      $info = @getimagesize($path);
+      if (empty($info)) {
+        throw new \Exception('invalid image');
+      }
+      if ($type == 'width') {
+        return $info[0];
+      } else {
+        return $info[1];
+      }
+    } catch (\Exception $e) {
+      return null;
+    }
+  }
+
+
+
+  public function secret($str) {
+    $str = str_rot13($str);
+    $str = str_shuffle($str);
+
+    return $str;
+  }
+
+
+
+  public function strPad($str, $padLength, $padString, $orient = STR_PAD_LEFT) {
+    return str_pad($str, $padLength, $padString, $orient);
+  }
+
+
+
+  public function br2nl($str) {
+    $breaks = ["<br />", "<br>", "<br/>"];
+    $str = str_ireplace($breaks, "\r\n", $str);
+
+    return $str;
+  }
+
+
+
+  public function joinByKey($arr, $glue = ',', $index = null) {
+    if (!is_array($arr) || empty($index)) {
+      return $arr;
+    }
+    $tmp = [];
+    foreach ($arr as $item) {
+      foreach ($item as $key => $val) {
+        if ($key == $index) {
+          $tmp[] = $val;
+        }
+      }
+    }
+    $ret = implode($glue, $tmp);
+
+    return $ret;
+  }
+
+
+
+  public function getName() {
+    return 'handy_extension';
+  }
+
+}
